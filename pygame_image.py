228fcec17951ae3232105de325990a354cb711b4
c0b22177 <c0b2217738@edu.teu.ac.jp>
2023-09-26 15:35:18 +0900

    練習問題3

--- pygame_image.py
+++ pygame_image.py
@@ -9,6 +9,9 @@ def main():
     bg_img = pg.image.load("ex01-20230926/fig/pg_bg.jpg")
     tori_img = pg.image.load("ex01-20230926/fig/3.png")
     tori_img = pg.transform.flip(tori_img, True, False)
+    tori_img2 = pg.image.load("ex01-20230926/fig/3.png")
+    tori_img2 = pg.transform.flip(tori_img, True, False)
+    tori_img2 = pg.transform.rotozoom(tori_img, 10, 1.0)
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -16,6 +19,7 @@ def main():
 
         screen.blit(bg_img, [0, 0])
         screen.blit(tori_img, [10,10])
+        screen.blit(tori_img2, [50,50])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
