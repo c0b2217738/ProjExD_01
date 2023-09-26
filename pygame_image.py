a6f53b3adeaa209392ed9af68e17288914782b91
c0b22177 <c0b2217738@edu.teu.ac.jp>
2023-09-26 15:47:45 +0900

    練習問題5

--- pygame_image.py
+++ pygame_image.py
@@ -9,7 +9,7 @@ def main():
     bg_img = pg.image.load("ex01-20230926/fig/pg_bg.jpg")
     tori_img = pg.image.load("ex01-20230926/fig/3.png")
     tori_img = pg.transform.flip(tori_img, True, False)
-    tori_img2 = pg.transform.rotozoom(tori_img, 10, 1.0)
+    tori_img2 = pg.transform.rotozoom(tori_img, -10, 1.0)
     tori_imgs = [tori_img, tori_img2]
     tmr = 0
     while True:
@@ -17,8 +17,7 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(tori_img, [10,10])
-        screen.blit(tori_img2, [50,50])
+        screen.blit(tori_imgs[tmr%2], [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
